513a5351aaf4f6ca55589919845ca6d580329f32
Obijuru <158084677+Obijuru@users.noreply.github.com>
2025-01-25 17:00:11 +0100

    add projects

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -8,10 +8,10 @@ const Projects = () => {
     <div className='mt-28'>
       <div className='font-Tangerine ml-10 text-3xl font-bold'>My <span className='text-green-600'>Projects</span></div>
 
-      <div className='mt-28 opacity-80 flex flex-col gap-20 mx-10'>
+      <div className='mt-28 opacity-80 flex flex-col gap-16 mx-10'>
           <div className='relative'>
           <img src={Token} />
-          <div className='absolute inset-0 flex gap-5 items-end left-6 bottom-3'>
+          <div className='absolute inset-0 flex flex-wrap gap-5 items-end left-6 bottom-3'>
             <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>React</div>
             <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>Motoko</div>
             <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>Tailwind CSS</div>
