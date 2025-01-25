54dc98c65fb057e8189b7231ea0fe6ec4098fd0b
Obijuru <158084677+Obijuru@users.noreply.github.com>
2025-01-25 17:05:53 +0100

    add projects

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
       <div className='mt-28 opacity-80 flex flex-col gap-16 mx-10'>
           <div className='relative'>
           <img src={Token} />
-          <div className='absolute inset-0 flex gap-5 items-end left-6 bottom-3'>
+          <div className='absolute inset-0 flex gap-5 items-end left-3 bottom-3'>
             <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>React</div>
             <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>Motoko</div>
             <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine whitespace-nowrap'>Tailwind CSS</div>
@@ -20,7 +20,7 @@ const Projects = () => {
 
         <div  className='relative'>
           <img src={amazon} />
-          <div className='absolute inset-0 flex gap-5 items-end left-6 bottom-3'>
+          <div className='absolute inset-0 flex gap-5 items-end left-3 bottom-3'>
             <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>React</div>
             <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>Tailwind CSS</div>
           </div>
@@ -28,7 +28,7 @@ const Projects = () => {
 
         <div  className='relative'>
           <img src={Note} />
-          <div className='absolute inset-0 flex gap-5 items-end left-6 bottom-3'>
+          <div className='absolute inset-0 flex gap-5 items-end left-3 bottom-3'>
             <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>React</div>
             <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>Tailwind CSS</div>
           </div>
