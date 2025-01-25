88349107d1228d518fcf06d59cca9c86980c1fa9
Obijuru <158084677+Obijuru@users.noreply.github.com>
2025-01-25 17:15:54 +0100

    add projects

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -9,32 +9,41 @@ const Projects = () => {
       <div className='font-Tangerine ml-10 text-3xl font-bold'>My <span className='text-green-600'>Projects</span></div>
 
       <div className='mt-28 opacity-80 flex flex-col gap-16 mx-10'>
+        <div>
           <div className='relative'>
-          <img src={Token} />
-          <div className='absolute inset-0 flex gap-5 items-end left-3 bottom-3'>
-            <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>React</div>
-            <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>Motoko</div>
-            <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine whitespace-nowrap'>Tailwind CSS</div>
-          </div>
+            <img src={Token} />
+              <div className='absolute inset-0 flex gap-5 items-end left-3 bottom-3'>
+                <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>React</div>
+                <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>Motoko</div>
+                <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine whitespace-nowrap'>Tailwind CSS</div>
+              </div>
+            </div>
+          <div className='text-light mt-5 flex justify-center font-Imperial'>Token Built on the ICP Blockchain</div>
         </div>
 
-        <div  className='relative'>
-          <img src={amazon} />
-          <div className='absolute inset-0 flex gap-5 items-end left-3 bottom-3'>
-            <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>React</div>
-            <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>Tailwind CSS</div>
+        <div>
+          <div  className='relative'>
+            <img src={amazon} />
+            <div className='absolute inset-0 flex gap-5 items-end left-3 bottom-3'>
+              <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>React</div>
+              <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>Tailwind CSS</div>
+            </div>
           </div>
+          <div className='text-light mt-5 flex justify-center font-Imperial'>Fully Functional E-commerce Website</div>
         </div>
 
-        <div  className='relative'>
-          <img src={Note} />
-          <div className='absolute inset-0 flex gap-5 items-end left-3 bottom-3'>
-            <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>React</div>
-            <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>Tailwind CSS</div>
+        <div>
+          <div  className='relative'>
+            <img src={Note} />
+            <div className='absolute inset-0 flex gap-5 items-end left-3 bottom-3'>
+              <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>React</div>
+              <div className='bg-light px-4 py-1 rounded-full text-purple-700 font-Tangerine'>Tailwind CSS</div>
+            </div>
           </div>
+          <div className='text-light mt-5 flex justify-center font-Imperial'>Note to Take Record</div>
         </div>
-
       </div>
+
     </div>
   )
 }
