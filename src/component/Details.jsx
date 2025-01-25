c8abc1b76ee8d4108521cbd4ce7597569634f429
Obijuru <158084677+Obijuru@users.noreply.github.com>
2025-01-25 17:40:42 +0100

    correction

--- src/component/Details.jsx
+++ src/component/Details.jsx
@@ -12,7 +12,7 @@ const Details = () => {
       <div className='xl:animate-slideright animate-slideup'>
       <div className='text-6xl font-playFair text-light h-[50vh] mx-3'>
         <div>Hello, Connect With <span className='text-green-700 font-Quicksand'>Daniel</span>,</div>
-        <div className='text-xl mt-5 font-Imperial'>A Frontend Developer and A Web3 Developer (Motoko, ICP Blockchain).</div>
+        <div className='text-xl mt-5 font-Chewy'>A Frontend Developer and A Web3 Developer (Motoko, ICP Blockchain).</div>
       </div>
 
       <div className='font-Quicksand text-lg ml-3'>I'm your ultimate companion to make a lasting impression in digital realm.</div>
