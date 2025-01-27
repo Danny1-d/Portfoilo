290eb735f88e9c47887aa72b227c034e3932c730
Obijuru <158084677+Obijuru@users.noreply.github.com>
2025-01-27 18:05:38 +0100

    width

--- src/component/Advantage.jsx
+++ src/component/Advantage.jsx
@@ -8,10 +8,10 @@ const Advantage = () => {
     <div id='/advantage' className='mt-28'>
       <div className='ml-10 text-3xl font-bold font-Tangerine'>MY <span className='text-green-600'>Advantage</span></div>
 
-      <div className='grid grid-cols-2 sm:grid-cols-3 mt-20 sm:gap-4 gap-2'>
+      <div className='grid grid-cols-2 sm:grid-cols-3 mt-20 sm:gap-4 gap-2 ml-3'>
 
         <div className='flex flex-col items-center'> 
-          <div className='border p-16 rounded-full flex flex-col items-center hover:border-green-500 w-44'>
+          <div className='border px-16 py-10 rounded-full flex flex-col items-center hover:border-green-500'>
             <img src={js} alt='js icon' className='rounded-xl'/>
             <div className='mt-10 font-bold text-xl text-green-700'>85%</div>
           </div>
@@ -19,7 +19,7 @@ const Advantage = () => {
         </div>
 
         <div className='flex flex-col items-center'>
-          <div className='border p-16 rounded-full flex flex-col items-center hover:border-green-500 w-44'>
+          <div className='border px-16 py-10 rounded-full flex flex-col items-center hover:border-green-500 w-44'>
             <img src={react} alt='js icon' className='rounded-xl'/>
             <div className='mt-10 font-bold text-xl text-green-700'>75%</div>
           </div>
@@ -27,7 +27,7 @@ const Advantage = () => {
         </div>
 
         <div className='flex flex-col items-center'>
-          <div className='border p-16 rounded-full flex flex-col items-center hover:border-green-500 w-44'>
+          <div className='border px-16 py-10  rounded-full flex flex-col items-center hover:border-green-500 w-44'>
             <img src={motoko} alt='js icon' className='rounded-xl'/>
             <div className='mt-10 font-bold text-xl text-green-700'>70%</div>
           </div>
