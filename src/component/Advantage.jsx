6fbba9abdd64f5d354398d0984b21c5744a8b002
Obijuru <158084677+Obijuru@users.noreply.github.com>
2025-01-27 19:16:43 +0100

    update

--- src/component/Advantage.jsx
+++ src/component/Advantage.jsx
@@ -8,11 +8,11 @@ const Advantage = () => {
     <div id='/advantage' className='mt-28'>
       <div className='ml-10 text-3xl font-bold font-Tangerine'>MY <span className='text-green-600'>Advantage</span></div>
 
-      <div className='grid grid-cols-2 sm:grid-cols-3 mt-20 sm:gap-4 gap-2 ml-3'>
+      <div className='grid grid-cols-2 sm:grid-cols-3 mt-20 sm:gap-4 gap-2'>
 
         <div className='flex flex-col items-center'> 
           <div className='border px-16 py-10 rounded-full flex flex-col items-center hover:border-green-500'>
-            <img src={js} alt='js icon' className='rounded-xl'/>
+            <img src={js} alt='js icon' className='rounded-xl '/>
             <div className='mt-10 font-bold text-xl text-green-700'>85%</div>
           </div>
           <div className='mt-7 text-light text-xl font-bold font-Tangerine'>JavaScript</div>
