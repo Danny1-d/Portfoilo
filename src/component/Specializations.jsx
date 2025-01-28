8b5631fae155c8ca508d883bdaf98dbde480e028
Obijuru <158084677+Obijuru@users.noreply.github.com>
2025-01-28 05:23:19 +0100

    add animation

--- src/component/Specializations.jsx
+++ src/component/Specializations.jsx
@@ -6,7 +6,7 @@ const Specializations = () => {
     <div className='overflow-hidden'>
     <motion.div id='/service' className='mt-20'
    whileInView={{ opacity: 1, x:0}} 
-    initial={{ opacity : 0, x:'-50vw'}} 
+    initial={{ opacity : 0, x:'50vw'}} 
     transition={{duration: 2}}>
       <div className='ml-10 mb-10 text-4xl font-Tangerine font-bold'>My <span className='text-green-800 ml-2 '> Discipline </span></div>
       <div className='border p-16 mr-2 rounded-xl hover:border-green-600'>
